f73177dc250b24adfd4ada4bde3a6c24a0571bdd
projectfinalaudio <irfanghat@gmail.com>
2023-09-26 13:20:37 +0300

    9/26/23: Added future modifications and code formatting

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import * as axios from "axios";
-import { fa, faker } from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 import * as https from "https";
 
 const instance = axios.default.create({
@@ -7,12 +7,11 @@ const instance = axios.default.create({
     httpsAgent: new https.Agent({
         rejectUnauthorized: false,
     }),
-
 });
-const token ="eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN5c3RlbSIsImZ1bGxOYW1lIjoiU3lzdGVtIEFjY291bnQiLCJlbWFpbCI6InJ1bS1zeXN0ZW1AdGhoLWxsYy5jb20iLCJyb2xlIjoiU3VwZXIgQWRtaW4iLCJleHAiOjE2OTU4MDQwNzh9.J6fRa9_qq0OhrAdDi9YQLUidNHnaUvWlH-wHMi-S41oAXXNdqnw0LYw9Ey6O-Rj9-_JaGMk26YqhzYFJboNZ0A"
+
+const token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN5c3RlbSIsImZ1bGxOYW1lIjoiU3lzdGVtIEFjY291bnQiLCJlbWFpbCI6InJ1bS1zeXN0ZW1AdGhoLWxsYy5jb20iLCJyb2xlIjoiU3VwZXIgQWRtaW4iLCJleHAiOjE2OTU4MDQwNzh9.J6fRa9_qq0OhrAdDi9YQLUidNHnaUvWlH-wHMi-S41oAXXNdqnw0LYw9Ey6O-Rj9-_JaGMk26YqhzYFJboNZ0A"
 
 async function seedData() {
-
     try {
         const url = new URL("https://localhost:5001/resources");
         for (let i = 0; i < 100; i++) {
@@ -22,10 +21,36 @@ async function seedData() {
                 firstName: faker.person.firstName(),
                 lastName: faker.person.lastName(),
                 email: faker.internet.email(),
+                /* 
+                
+                Implement Ids using an array in future releases e.g 
+            
+                    let resourceTypeIds: string[] = [
+                        '9c480954-f4a0-4560-ae87-9da1bf47a676', 
+                        '45b8224f-97b5-4fc1-810f-43e6fe94a87d',
+                        '4e5bbade-a8da-4afb-a397-dd13b3ef20b9'
+                    ]
+
+                    let resourceLevelds: string[] = [
+                        '9c480954-f4a0-4560-ae87-9da1bf47a676', 
+                        '45b8224f-97b5-4fc1-810f-43e6fe94a87d',
+                        '4e5bbade-a8da-4afb-a397-dd13b3ef20b9'
+                    ]
+
+                    let locationIds: string[] = [
+                        '9c480954-f4a0-4560-ae87-9da1bf47a676', 
+                        '45b8224f-97b5-4fc1-810f-43e6fe94a87d',
+                        '4e5bbade-a8da-4afb-a397-dd13b3ef20b9'
+                    ]
+
+                Then set the current value on iteration.
+
+                */
                 resourceTypeId: "9c480954-f4a0-4560-ae87-9da1bf47a676",
                 resourceLevelId: "3b56aa69-82d9-4953-8283-888fdd7d7896",
                 locationId: "092ca8fb-edd0-4cb7-9e45-e7af50a8aabf",
             };
+
             const respose = await instance.post(url.href, payload, {
                 headers: {
                     Authorization: `Bearer ${token}`,
@@ -33,10 +58,11 @@ async function seedData() {
                     Accept: "*/*",
                 },
             });
+
             console.log(respose.status);
         }
     } catch (error) {
-        console.log("error", error);
+        console.log("ERROR", error.Message);
     }
 }
 
